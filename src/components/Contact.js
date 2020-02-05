27dd64d951485163b6e40716f49ccb36ac986402
James Vo <jv@ualberta.ca>
2020-02-04 23:11:59 -0700

    cleanup

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,41 +3,7 @@ import React, { Component } from 'react';
 class Contact extends Component {
     render() {
         return(
-            <section id='contact'>
-                
-                <div className="row">
-    <div class="col">
-      1 of 2
-    </div>
-    <div class="col">
-      2 of 2
-    </div>
-  </div>
-  <div class="row">
-    <div class="col">
-      1 of 3
-    </div>
-    <div class="col">
-      2 of 3
-    </div>
-    <div class="col">
-      3 of 3
-    </div>
-  </div>
-
-<div class="container">
-  <div class="row">
-    <div class="col-sm">
-      One of three columns
-    </div>
-    <div class="col-sm">
-      One of three columns
-    </div>
-    <div class="col-sm">
-      One of three columns
-    </div>
-  </div>
-</div>    
+            <section id='contact'>  
             </section>
         )
     }
